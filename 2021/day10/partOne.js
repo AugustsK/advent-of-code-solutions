9d8f91b8e68f2fd7bd8a720e7eca09adcde3f9a3
Augusts Karklins <augusts.karklins@vaimo.com>
2021-12-10 08:37:16 +0200

    2021, day 10

--- 2021/day10/partOne.js
+++ 2021/day10/partOne.js
@@ -2,8 +2,43 @@ const { getInput, strToLines, lineToArr, lineToIntArr, strToEmptyLineGroups } = 
 const { arrDiff, arrIntersects } = require('../../utils/array');
 const { outResult, outDebug } = require('../../utils/output');
 
+const map = str => str
+    .replace(/\(/g, 'a')
+    .replace(/\)/g, 'b')
+    .replace(/\[/g, 'c')
+    .replace(/]/g, 'd')
+    .replace(/{/g, 'e')
+    .replace(/}/g, 'f')
+    .replace(/</g, 'g')
+    .replace(/>/g, 'h');
+
+const pairs = { a: 'b', c: 'd', e: 'f', g: 'h' };
+const points = { b: 3, d: 57, f: 1197, h: 25137 };
+const lines = strToLines(getInput()).map(line => map(line));
 let result = 0;
 
-// solution goes here
+const validate = (str, bufferArg = []) => {
+    const char = str.slice(0, 1);
+    const remainder = str.slice(1);
+    const buffer = [...bufferArg];
+
+    if (['a', 'c', 'e', 'g'].includes(char)) {
+        buffer.push(char);
+
+        if (remainder.length) return validate(remainder, buffer);
+        else return 0;
+    } else {
+        const toCheck = buffer.pop();
+
+        if (pairs[toCheck] === char) {
+            if (remainder.length) return validate(remainder, buffer);
+            else return 0;
+        } else {
+            return points[char];
+        }
+    }
+}
+
+lines.forEach(line => result += validate(line));
 
 outResult(result);
--- 2021/day10/partTwo.js
+++ 2021/day10/partTwo.js
@@ -2,8 +2,49 @@ const { getInput, strToLines, lineToArr, lineToIntArr, strToEmptyLineGroups } = 
 const { arrDiff, arrIntersects } = require('../../utils/array');
 const { outResult, outDebug } = require('../../utils/output');
 
-let result = 0;
+const map = str => str
+        .replace(/\(/g, 'a')
+        .replace(/\)/g, 'b')
+        .replace(/\[/g, 'c')
+        .replace(/]/g, 'd')
+        .replace(/{/g, 'e')
+        .replace(/}/g, 'f')
+        .replace(/</g, 'g')
+        .replace(/>/g, 'h');
 
-// solution goes here
+const pairs = { a: 'b', c: 'd', e: 'f', g: 'h' };
+const points = { b: 1, d: 2, f: 3, h: 4 }
+const lines = strToLines(getInput()).map(line => map(line));
+const calculate = buffer => buffer.reverse().reduce((sum, char) => sum * 5 + points[pairs[char]], 0);
 
-outResult(result);
+const validate = (str, bufferArg = []) => {
+    const char = str.slice(0, 1);
+    const remainder = str.slice(1);
+    const buffer = [...bufferArg];
+
+    if (['a', 'c', 'e', 'g'].includes(char)) {
+        buffer.push(char);
+
+        if (remainder.length) return validate(remainder, buffer);
+        else return calculate(buffer);
+    } else {
+        const toCheck = buffer.pop();
+
+        if (pairs[toCheck] === char) {
+            if (remainder.length) return validate(remainder, buffer);
+            else if (buffer.length) return calculate(buffer);
+        }
+    }
+
+    return false
+}
+
+let scores = [];
+
+lines.forEach(line => {
+    const score = validate(line);
+
+    if (score) scores.push(score);
+});
+
+outResult(scores.sort((a, b) => a < b ? -1 : b > a ? 1 : 0)[Math.floor(scores.length / 2)]);
