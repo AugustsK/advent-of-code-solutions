3c8f2ca7f46b75810df5b6942b93e4317485959c
Augusts Karklins <augusts.karklins@vaimo.com>
2021-12-13 08:33:35 +0200

    2021, day 13

---
+++ 2021/day13/partOne.js
@@ -0,0 +1,46 @@
+const { getInput, strToLines, lineToArr, lineToIntArr, strToEmptyLineGroups } = require('../../utils/input');
+const { arrIntersects, arrDiff, countInstances, sort } = require('../../utils/array');
+const { outResult, outDebug } = require('../../utils/output');
+
+let dots = strToLines(getInput()).filter(line => /^[0-9]/.test(line)).map(line => {
+    const [ x, y ] = line.split(',').map(x => parseInt(x, 10));
+
+    return { x, y };
+});
+const instructions = strToLines(getInput()).filter(line => /^fold/.test(line)).map(line => {
+    const [ axis, amount ] = line.replace('fold along ', '').split('=');
+
+    return {
+        axis,
+        amount: parseInt(amount, 10)
+    }
+});
+
+const filterDots = () => {
+    const unique = new Set();
+
+    dots = dots.filter(dot => {
+        const dotStr = `${dot.x}:${dot.y}`;
+
+        if (unique.has(dotStr)) return false;
+        else {
+            unique.add(dotStr);
+
+            return true;
+        }
+    });
+}
+
+const fold = (axis, amount) => {
+    dots = dots.map(dot => {
+        return {
+            x: axis === 'x' ? (dot.x > amount ? dot.x - (dot.x - amount) * 2 : dot.x) : dot.x,
+            y: axis === 'y' ? (dot.y > amount ? dot.y - (dot.y - amount) * 2 : dot.y) : dot.y
+        }
+    });
+
+    filterDots();
+}
+
+fold(instructions[0].axis, instructions[0].amount);
+outResult(dots.length);
---
+++ 2021/day13/partTwo.js
@@ -0,0 +1,73 @@
+const { getInput, strToLines, lineToArr, lineToIntArr, strToEmptyLineGroups } = require('../../utils/input');
+const { arrIntersects, arrDiff, countInstances, sort } = require('../../utils/array');
+const { outResult, outDebug } = require('../../utils/output');
+
+let dots = strToLines(getInput()).filter(line => /^[0-9]/.test(line)).map(line => {
+    const [ x, y ] = line.split(',').map(x => parseInt(x, 10));
+
+    return { x, y };
+});
+const instructions = strToLines(getInput()).filter(line => /^fold/.test(line)).map(line => {
+    const [ axis, amount ] = line.replace('fold along ', '').split('=');
+
+    return {
+        axis,
+        amount: parseInt(amount, 10)
+    }
+});
+
+const filterDots = () => {
+    const unique = new Set();
+
+    dots = dots.filter(dot => {
+        const dotStr = `${dot.x}:${dot.y}`;
+
+        if (unique.has(dotStr)) return false;
+        else {
+            unique.add(dotStr);
+
+            return true;
+        }
+    });
+}
+
+const fold = (axis, amount) => {
+    dots = dots.map(dot => {
+        return {
+            x: axis === 'x' ? (dot.x > amount ? dot.x - (dot.x - amount) * 2 : dot.x) : dot.x,
+            y: axis === 'y' ? (dot.y > amount ? dot.y - (dot.y - amount) * 2 : dot.y) : dot.y
+        }
+    });
+
+    filterDots();
+}
+
+instructions.forEach(({ axis, amount }) => {
+    fold(axis, amount);
+});
+
+let grid = [];
+let highestY = 0;
+let highestX = 0;
+
+dots.forEach(({ x, y }) => {
+    highestX = x > highestX ? x : highestX;
+    highestY = y > highestY ? y : highestY;
+});
+
+for (let y = 0; y <= highestY; y++) {
+    grid.push([]);
+
+    for (let x = 0; x <= highestX; x++) {
+        grid[y].push('.');
+    }
+}
+
+dots.forEach(({ x, y }) => {
+    grid[y][x] = '#';
+});
+
+grid = grid.map(line => line.join(''));
+grid = grid.join('\n');
+
+console.log(grid);
