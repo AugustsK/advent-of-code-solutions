776dd5522ddf37c9c1bab5012fb14c041f0041d6
Augusts Karklins <augusts.karklins@vaimo.com>
2021-12-17 00:30:42 +0200

    update template

--- template/partTwo.js
+++ template/partTwo.js
@@ -1,5 +1,6 @@
 const { getInput, strToLines, lineToArr, lineToIntArr, strToEmptyLineGroups } = require('../../utils/input');
 const { arrIntersects, arrDiff, countInstances, sort } = require('../../utils/array');
+const { lcm, gcd } = require('../../utils/math');
 const { outResult, outDebug, outProgress } = require('../../utils/output');
 
 let result = 0;
