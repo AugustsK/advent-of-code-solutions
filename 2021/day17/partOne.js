eb47331c36459bda385727bcb59c2c3f72649a68
Augusts Karklins <augusts.karklins@vaimo.com>
2021-12-17 18:43:05 +0200

    remove unnecessary partOne code on day 17 of 2021

--- 2021/day17/partOne.js
+++ 2021/day17/partOne.js
@@ -3,26 +3,9 @@ const { arrIntersects, arrDiff, countInstances, sort } = require('../../utils/ar
 const { lcm, gcd, gauss } = require('../../utils/math');
 const { outResult, outDebug, outProgress } = require('../../utils/output');
 
-const [ xInput, yInput ] = getInput().split(', ');
-const [ , xRange ] = xInput.split('=');
-const [ xMin, xMax ] = xRange.split('..').map(x => parseInt(x, 10));
+const [ , yInput ] = getInput().split(', ');
 const [ , yRange ] = yInput.split('=');
-const [ yMin, yMax ] = yRange.split('..').map(y => parseInt(y, 10));
-
-const findFactorial = (target, overshoot = true) => {
-    let result = 2;
-    let factorial = gauss(2);
-
-    while (factorial < target) {
-        factorial = gauss(++result);
-    }
-
-    return overshoot ? result : result - 1;
-}
-
-// Trajectories that stop on X axis within bounds
-const minXTrajectory = findFactorial(xMin, true);
-const maxXTrajectory = findFactorial(xMax, false);
+const [ yMin, ] = yRange.split('..').map(y => parseInt(y, 10));
 
 let result = gauss(Math.abs(yMin) - 1);
 
