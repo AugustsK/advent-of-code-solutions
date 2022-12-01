df7226de813dd8d69650e1b0fb5762c9698fff72
Augusts Kārkliņš <augusts.karklins@magebit.com>
2022-12-01 09:53:28 +0200

    aoc 2022, day01

---
+++ 2022/day01/index.js
@@ -0,0 +1,21 @@
+const Utils = require('../../utils');
+const {sort} = require("../../utils/array");
+
+const elves = Utils.Input
+    .getInput()
+    .split(/\n\n/)
+    .map(
+        elve => elve
+            .split(/\n/)
+            .filter(calories => !!calories)
+            .map(calories => parseInt(calories, 10))
+            .reduce((previousValue, currentValue) => previousValue + currentValue, 0)
+    );
+
+const sortedElves = [...elves].sort((a, b) => b - a);
+
+let partOne = sortedElves[0];
+let partTwo = sortedElves.slice(0, 3).reduce((previousValue, currentValue) => previousValue + currentValue, 0);
+
+Utils.Output.outResult(partOne);
+Utils.Output.outResult(partTwo);
