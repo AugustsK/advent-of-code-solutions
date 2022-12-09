a4ffda8d24b8aa958c9d997a8249111071bd9d44
Augusts Kārkliņš <augusts.karklins@magebit.com>
2022-12-09 13:14:04 +0200

    2022, day09 part2

--- 2022/day09/index.js
+++ 2022/day09/index.js
@@ -8,31 +8,50 @@ const LEFT = 'L';
 const coords = (x, y) => `${x}-${y}`;
 
 const tailVisits = new Set([coords(0, 0)]);
+const longTailVisits = new Set([coords(0, 0)]);
 let H = [0, 0];
 let T = [0, 0];
+let knots = [
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0],
+    [0, 0]
+];
 
-const resolveT = () => {
-    const [hX, hY] = H;
-    const [tX, tY] = T;
-    const xDiffAbs = Math.abs(hX - tX);
-    const yDiffAbs = Math.abs(hY - tY);
-    let xDiff = 0;
-    let yDiff = 0;
+const resolveT = (head, knots) => {
+    let curHead = head;
 
-    if (xDiffAbs > 1 && yDiffAbs === 0) {
-        xDiff = (hX - tX) / 2;
-    } else if (yDiffAbs > 1 && xDiffAbs === 0) {
-        yDiff = (hY - tY) / 2;
-    } else if (xDiffAbs > 1 && yDiffAbs === 1) {
-        xDiff = (hX - tX) / 2;
-        yDiff = hY - tY;
-    } else if (yDiffAbs > 1 && xDiffAbs === 1) {
-        xDiff = hX - tX;
-        yDiff = (hY - tY) / 2;
-    }
+    return knots.map(curKnot => {
+        const [hX, hY] = curHead;
+        const [tX, tY] = curKnot;
+        const xDiffAbs = Math.abs(hX - tX);
+        const yDiffAbs = Math.abs(hY - tY);
+        let xDiff = 0;
+        let yDiff = 0;
 
-    T = [tX + xDiff, tY + yDiff];
-    tailVisits.add(coords(...T));
+        if (xDiffAbs > 1) {
+            xDiff = (hX - tX) / 2;
+        } else if (xDiffAbs === 1 && yDiffAbs > 1) {
+            xDiff = hX - tX;
+        }
+
+        if (yDiffAbs > 1) {
+            yDiff = (hY - tY) / 2;
+        } else if (yDiffAbs === 1 && xDiffAbs > 1) {
+            yDiff = hY - tY;
+        }
+
+        const newKnot = [tX + xDiff, tY + yDiff];
+
+        curHead = [...newKnot];
+
+        return newKnot;
+    });
 }
 
 const move = (dir, amount) => {
@@ -43,25 +62,26 @@ const move = (dir, amount) => {
     switch (dir) {
         case UP:
             H = [hX, hY - 1];
-            resolveT();
 
             break;
         case RIGHT:
             H = [hX + 1, hY];
-            resolveT();
 
             break;
         case DOWN:
             H = [hX, hY + 1];
-            resolveT();
 
             break;
         case LEFT:
             H = [hX - 1, hY];
-            resolveT();
 
             break;
     }
+
+    T = resolveT(H, [T])[0];
+    knots = resolveT(H, knots);
+    tailVisits.add(coords(...T));
+    longTailVisits.add(coords(...knots[knots.length - 1]));
 
     move(dir, amount - 1);
 }
@@ -74,7 +94,7 @@ Utils.Input.strToLines(Utils.Input.getInput()).filter(line => line.length).forEa
 });
 
 let partOne = tailVisits.size;
-let partTwo = 0;
+let partTwo = longTailVisits.size;
 
 Utils.Output.outResult(partOne);
 Utils.Output.outResult(partTwo);
